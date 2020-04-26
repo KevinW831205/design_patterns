9b9a01934d9433de06c470b6a49ddb69d8ba60f7
KevinW <bird831205@hotmail.com>
2020-04-25 20:03:57 -0400

    looking at string builder

---
+++ src/main/java/com/github/kevinw831205/creational/builder/JavaBuilders.java
@@ -0,0 +1,20 @@
+package com.github.kevinw831205.creational.builder;
+
+public class JavaBuilders {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        System.out.println("<p>" + hello + "</p>");
+        String[] words = {"hello", "world"};
+        System.out.println("<ul>" + ".....");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<ul>\n");
+        for (String word : words) {
+            sb.append(String.format("  <li>%s</li>\n", word));
+        }
+        sb.append("</ul>");
+        System.out.println(sb.toString());
+
+    }
+
+}
