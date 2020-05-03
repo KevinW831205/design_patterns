bc92f96831d56d7fbcee20b43f68a3221e8d9e00
KevinW <bird831205@hotmail.com>
2020-05-02 21:37:47 -0400

    making lazy singleton thread safe

--- src/main/java/com/github/kevinw831205/creational/singleton/LazySingleton.java
+++ src/main/java/com/github/kevinw831205/creational/singleton/LazySingleton.java
@@ -3,14 +3,26 @@ package com.github.kevinw831205.creational.singleton;
 public class LazySingleton {
     private static LazySingleton INSTANCE;
 
-    private LazySingleton(){
+    private LazySingleton() {
         System.out.println("initialize lazy singleton");
     }
 
     // multi threading doesn't work well
-    public static LazySingleton getINSTANCE(){
-        if (INSTANCE == null){
-            INSTANCE =  new LazySingleton();
+//    public static synchronized LazySingleton getINSTANCE(){
+//        if (INSTANCE == null){
+//            INSTANCE =  new LazySingleton();
+//        }
+//        return INSTANCE;
+//    }
+
+    //doub checked locking
+    public static LazySingleton getINSTANCE() {
+        if (INSTANCE == null) {
+            synchronized (LazySingleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new LazySingleton();
+                }
+            }
         }
         return INSTANCE;
     }
