83233a7feee8a9178581dafef99db3f925bebcb8
KevinW <bird831205@hotmail.com>
2020-05-05 23:10:31 -0400

    composite pattern in neural network

---
+++ src/main/java/com/github/kevinw831205/structural/composite/NeuralNetworkDemo.java
@@ -0,0 +1,21 @@
+package com.github.kevinw831205.structural.composite;
+
+public class NeuralNetworkDemo {
+
+    public static void main(String[] args) {
+        Neuron neuron1 = new Neuron();
+        Neuron neuron2 = new Neuron();
+
+        NeuronLayer layer1 = new NeuronLayer();
+        NeuronLayer layer2 = new NeuronLayer();
+
+        // connecting neurons
+//        neuron1.connectTo(neuron2);
+        // not implmented
+//        neuron1.connectTo(layer1);
+//        layer1.connectTo(neuron1);
+//        layer1.connectTon(layer2);
+
+
+    }
+}
---
+++ src/main/java/com/github/kevinw831205/structural/composite/NeuronLayer.java
@@ -0,0 +1,6 @@
+package com.github.kevinw831205.structural.composite;
+
+import java.util.ArrayList;
+
+public class NeuronLayer extends ArrayList<Neuron> implements Iterable<Neuron> {
+}
